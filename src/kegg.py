af2b280d7fa562e157724d8b0a16065f45743e70
Lucas Diedrich <lucas.diedrich@hotmail.de>
2022-10-13 11:38:39 +0200

    Added code for KEGG retrieval and preprocessing

--- src/kegg.py
+++ src/kegg.py
@@ -1,1 +1,81 @@
 #!/usr/bin/env python 
+
+import pandas as pd 
+import tqdm
+
+def kegg_compounds(): 
+    """ 
+    Gets IDs of all KEGG compounds with their name 
+
+    RETURNS 
+    -------
+    :class:`pd.DataFrame`
+        Pandas Dataframe with column of KEGG ID and name of compound 
+    """
+    # Get compound list from https://rest.kegg.jp/list/compound
+    df = pd.read_csv('https://rest.kegg.jp/list/compound', sep = '\t', names = ['id', 'name'])
+    # ids are returned in the form cpd:ID (e.g. cpd:C00931). Remove leading cpd:
+    df['id'] = df['id'].apply(lambda x: x.split(':')[1])
+    # Only store first name 
+    df['name'] = df['name'].apply(lambda x: x.split(';')[0])
+    
+    return df 
+
+    
+def keggID_from_molformula(molecular_formula:str):
+    """ 
+    Uses KEGG API to return a dataframe that stores all KEGG IDs whose molecular formulas match a query. 
+
+    INPUT
+    ----- 
+    molecular_formula (`str`)
+        Molecular formula of molecule of interest
+
+    RETURNS 
+    ------- 
+    :class:`pd.DataFrame`
+
+    EXAMPLE 
+    ------- 
+    >>> keggID_from_molformula('H2O')
+       id molecular_formula
+    0  C00001               H2O
+    """
+
+    # KEGG API 
+    request = f'https://rest.kegg.jp/find/compound/{molecular_formula}/formula'
+
+    # Use web-retrieval tool of pandas and read in as dataframe 
+    df = pd.read_csv(request, sep='\t', names = ['id', 'molecular_formula'])
+
+    # Only consider entries with exact match of molecular formula  
+    df = df.loc[df['molecular_formula'] == molecular_formula]
+
+    # ids are returned in the form cpd:ID (e.g. cpd:C00931). Remove leading cpd:
+    df['id']= df['id'].apply(lambda x: x.split(':')[1])
+    
+    return df 
+
+
+def kegg_batch_retrieval(molecular_formulas:List[str]): 
+    """ 
+    Retrieves information for several molecular formulas that are passed as list and returns a single dataframe 
+
+    EXAMPLE 
+    -------
+    >>> molformulas = ['H2O', 'C6H12O6', 'C6H8O7']
+    >>> df = kegg_batch_retrieval(molformulas)
+    >>> df.sample(3)
+            id molecular_formula
+    23  C01906           C6H12O6
+    46  C21050           C6H12O6
+    7   C00267           C6H12O6
+    
+    """
+    from tqdm import tqdm
+    
+    df_list = []
+    for i, mol in zip(tqdm(range(len(molecular_formulas))), molecular_formulas): 
+        df_list.append(keggID_from_molformula(mol)) 
+
+    return pd.concat(df_list, axis=0)
--- src/preprocess.py
+++ src/preprocess.py
@@ -1,4 +1,40 @@
 #!/usr/bin/env python 
 
+def parse_ranked_genes(df, pcutoff:float =1e-3, changecutoff:None|float = None, ion_column:str = 'names', logchange_column:str = 'logfoldchanges', pval_column:str = 'pvals_adj'): 
+    """ 
+    df (:class:`pd.DataFrame`)
+        Dataframe which is supposed to be parsed
+    pcutoff (float)
+        p value cutoff (default 0.001). If no cutoff is desired, set pcutoff = 1 
+    changecutoff (None|float)
+        Cutoff for change in abundance (default: None/Not performed)
+    ion_column (str)
+        Name of column that contains ion formulas (default: names) 
+    logchange_column (str)
+        Name of column that contains logfold changes of metabolites (default: logfoldchanges) 
+    pval_column (str)
+        Name of column that contains p values of changes of metabolites (default: pvals_adj) 
 
+
+    
+    EXAMPLE
+    -------
+    >>> df
+                   names     scores  logfoldchanges          pvals      pvals_adj
+    121    C10H10N2O3S-H   3.120556       -0.096448   1.805101e-03   2.758008e-03
+    241  C11H11ClN2O2+Cl  -2.799665       -6.435749   5.115568e-03   7.651858e-03
+    17         C11H9N3-H  29.209204        0.578870  1.481648e-187  2.776140e-186
+    >>> parse_ranked_genes(df)
+    
+    """
+
+    # Extract molecular formulas 
+    df['molecular_formula'] = df[ion_column].str.extract('(.*)[\+|\-].*') 
+
+    df = df.loc[df[pval_column] < pcutoff]
+
+    if changecutoff is not None: 
+        df = df.loc[df[pval_column] > changecutoff]
+    
+    return df 
 
