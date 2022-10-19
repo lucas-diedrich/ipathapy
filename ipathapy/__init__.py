9cde623ef66bf36c95b346bd8894e74bfbe0272e
Lucas Diedrich <lucas.diedrich@hotmail.de>
2022-10-19 16:47:56 +0200

    Rename colors.py to layout.py

--- ipathapy/__init__.py
+++ ipathapy/__init__.py
@@ -4,6 +4,6 @@ __author__ = 'Lucas Diedrich'
 __email__ = 'lucas.diedrich@embl.de'
 __version__ = '0.0.1'
 
-from . import preprocess, kegg, colors, ipath
+from . import preprocess, kegg, layout, ipath
 
-__all__ = ['preprocess', 'kegg', 'colors', 'ipath']
+__all__ = ['preprocess', 'kegg', 'layout', 'ipath']
